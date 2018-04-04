b515b515b40de175f2cbb0898e9355a0313b5044
James Golovich <james@gnuinter.net>
2018-04-03 22:37:10 -0700

    Allow permalink to be modified in editor

---
+++ src/ModifyPermalink.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DLM_Tweaks;
+
+class ModifyPermalink {
+
+	private $endpoint;
+
+	private $ep_value;
+
+	public function __construct() {
+		$this->endpoint = ( $endpoint = get_option( 'dlm_download_endpoint' ) ) ? $endpoint : 'download';
+		$this->ep_value = ( $ep_value = get_option( 'dlm_download_endpoint_value' ) ) ? $ep_value : 'ID';
+
+		add_filter( 'dlm_cpt_dlm_download_args', array( $this, 'download_cpt_args' ), 10, 1 );
+	}
+
+	function download_cpt_args( $args ) {
+		if ( 'slug' === $this->ep_value ) {
+			// Allow modifying slug in editor
+			$args['publicly_queryable'] = true;
+			$args['rewrite'] = array('slug' => $this->endpoint );
+		}
+		return $args;
+	}
+
+}
